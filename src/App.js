fc1c152c7fccb28504223b1b64d4c8b73acdfa0c
GauharRepo <tech.mdg11@gmail.com>
2025-07-14 15:53:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount]=useState(0);
+  return (
+    <div className="App">
+      <h>count :</h>{count}
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
